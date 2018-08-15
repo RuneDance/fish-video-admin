1ac1b6113075994c95c3a813a1123846e742b1f8
yuyongtao <smallbadoncelaugh@gmail.com>
2018-08-15 09:55:56 +0800

    modify by yyt

--- fish-video-service/src/main/java/com/fishvideo/service/impl/UsersServiceImpl.java
+++ fish-video-service/src/main/java/com/fishvideo/service/impl/UsersServiceImpl.java
@@ -1,59 +1,58 @@
 package com.fishvideo.service.impl;
 
-import java.util.List;
-
-import org.apache.commons.lang3.StringUtils;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-
-import com.github.pagehelper.PageHelper;
-import com.github.pagehelper.PageInfo;
 import com.fishvideo.mapper.UsersMapper;
 import com.fishvideo.pojo.Users;
 import com.fishvideo.pojo.UsersExample;
 import com.fishvideo.pojo.UsersExample.Criteria;
 import com.fishvideo.service.UsersService;
 import com.fishvideo.utils.PagedResult;
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class UsersServiceImpl implements UsersService {
 
-	@Autowired
-	private UsersMapper userMapper;
-	
-	@Override
-	public PagedResult queryUsers(Users user, Integer page, Integer pageSize) {
+    @Autowired
+    private UsersMapper userMapper;
 
-		String username = "";
-		String nickname = "";
-		if (user != null) {
-			username = user.getUsername();
-			nickname = user.getNickname();
-		}
-		
-		PageHelper.startPage(page, pageSize);
+    @Override
+    public PagedResult queryUsers(Users user, Integer page, Integer pageSize) {
 
-		UsersExample userExample = new UsersExample();
-		Criteria userCriteria = userExample.createCriteria();
-		if (StringUtils.isNotBlank(username)) {
-			userCriteria.andUsernameLike("%" + username + "%");
-		}
-		if (StringUtils.isNotBlank(nickname)) {
-			userCriteria.andNicknameLike("%" + nickname + "%");
-		}
+        String username = "";
+        String nickname = "";
+        if (user != null) {
+            username = user.getUsername();
+            nickname = user.getNickname();
+        }
 
-		List<Users> userList = userMapper.selectByExample(userExample);
+        PageHelper.startPage(page, pageSize);
 
-		PageInfo<Users> pageList = new PageInfo<Users>(userList);
+        UsersExample userExample = new UsersExample();
+        Criteria userCriteria = userExample.createCriteria();
+        if (StringUtils.isNotBlank(username)) {
+            userCriteria.andUsernameLike("%" + username + "%");
+        }
+        if (StringUtils.isNotBlank(nickname)) {
+            userCriteria.andNicknameLike("%" + nickname + "%");
+        }
 
-		PagedResult grid = new PagedResult();
-		grid.setTotal(pageList.getPages());
-		grid.setRows(userList);
-		grid.setPage(page);
-		grid.setRecords(pageList.getTotal());
+        List<Users> userList = userMapper.selectByExample(userExample);
 
-		return grid;
-	}
+        PageInfo<Users> pageList = new PageInfo<Users>(userList);
+
+        PagedResult grid = new PagedResult();
+        grid.setTotal(pageList.getPages());
+        grid.setRows(userList);
+        grid.setPage(page);
+        grid.setRecords(pageList.getTotal());
+
+        return grid;
+    }
 
 
 }
--- fish-video-service/src/main/java/com/fishvideo/service/impl/VideoServiceImpl.java
+++ fish-video-service/src/main/java/com/fishvideo/service/impl/VideoServiceImpl.java
@@ -1,15 +1,5 @@
 package com.fishvideo.service.impl;
 
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
-import org.n3r.idworker.Sid;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-
-import com.github.pagehelper.PageHelper;
-import com.github.pagehelper.PageInfo;
 import com.fishvideo.enums.BGMOperatorTypeEnum;
 import com.fishvideo.mapper.BgmMapper;
 import com.fishvideo.mapper.UsersReportMapperCustom;
@@ -22,96 +12,105 @@ import com.fishvideo.service.VideoService;
 import com.fishvideo.utils.JsonUtils;
 import com.fishvideo.utils.PagedResult;
 import com.fishvideo.web.util.ZKCurator;
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
+import org.n3r.idworker.Sid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @Service
 public class VideoServiceImpl implements VideoService {
 
-	@Autowired
-	private VideosMapper videosMapper;
-		
-	@Autowired
-	private BgmMapper bgmMapper;
-	
-	@Autowired
-	private Sid sid;
-	
-	@Autowired
-	private ZKCurator zkCurator;
-	
-	@Autowired
-	private UsersReportMapperCustom usersReportMapperCustom;
-	
-	@Override
-	public PagedResult queryReportList(Integer page, Integer pageSize) {
+    @Autowired
+    private VideosMapper videosMapper;
 
-		PageHelper.startPage(page, pageSize);
+    @Autowired
+    private BgmMapper bgmMapper;
 
-		List<Reports> reportsList = usersReportMapperCustom.selectAllVideoReport();
+    @Autowired
+    private Sid sid;
 
-		PageInfo<Reports> pageList = new PageInfo<Reports>(reportsList);
+    @Autowired
+    private ZKCurator zkCurator;
 
-		PagedResult grid = new PagedResult();
-		grid.setTotal(pageList.getPages());
-		grid.setRows(reportsList);
-		grid.setPage(page);
-		grid.setRecords(pageList.getTotal());
+    @Autowired
+    private UsersReportMapperCustom usersReportMapperCustom;
 
-		return grid;
-	}
+    @Override
+    public PagedResult queryReportList(Integer page, Integer pageSize) {
 
-	@Override
-	public void updateVideoStatus(String videoId, Integer status) {
-		
-		Videos video = new Videos();
-		video.setId(videoId);
-		video.setStatus(status);
-		videosMapper.updateByPrimaryKeySelective(video);
-	}
+        PageHelper.startPage(page, pageSize);
 
-	@Override
-	public PagedResult queryBgmList(Integer page, Integer pageSize) {
-		
-		PageHelper.startPage(page, pageSize);
-		
-		BgmExample example = new BgmExample();
-		List<Bgm> list = bgmMapper.selectByExample(example);
-		
-		PageInfo<Bgm> pageList = new PageInfo<>(list);
-		
-		PagedResult result = new PagedResult();
-		result.setTotal(pageList.getPages());
-		result.setRows(list);
-		result.setPage(page);
-		result.setRecords(pageList.getTotal());
-		
-		return result;
-	}
+        List<Reports> reportsList = usersReportMapperCustom.selectAllVideoReport();
 
-	@Override
-	public void addBgm(Bgm bgm) {
-		String bgmId = sid.nextShort();
-		bgm.setId(bgmId);
-		bgmMapper.insert(bgm);
-		
-		Map<String, String> map = new HashMap<>();
-		map.put("operType", BGMOperatorTypeEnum.ADD.type);
-		map.put("path", bgm.getPath());
-		
-		zkCurator.sendBgmOperator(bgmId, JsonUtils.objectToJson(map));
-	}
-	
-	@Override
-	public void deleteBgm(String id) {
-		Bgm bgm = bgmMapper.selectByPrimaryKey(id);
-		
-		bgmMapper.deleteByPrimaryKey(id);
-		
-		Map<String, String> map = new HashMap<>();
-		map.put("operType", BGMOperatorTypeEnum.DELETE.type);
-		map.put("path", bgm.getPath());
-		
-		zkCurator.sendBgmOperator(id, JsonUtils.objectToJson(map));
-		
-	}
+        PageInfo<Reports> pageList = new PageInfo<>(reportsList);
+
+        PagedResult grid = new PagedResult();
+        grid.setTotal(pageList.getPages());
+        grid.setRows(reportsList);
+        grid.setPage(page);
+        grid.setRecords(pageList.getTotal());
+
+        return grid;
+    }
+
+    @Override
+    public void updateVideoStatus(String videoId, Integer status) {
+
+        Videos video = new Videos();
+        video.setId(videoId);
+        video.setStatus(status);
+        videosMapper.updateByPrimaryKeySelective(video);
+    }
+
+    @Override
+    public PagedResult queryBgmList(Integer page, Integer pageSize) {
+
+        PageHelper.startPage(page, pageSize);
+
+        BgmExample example = new BgmExample();
+        List<Bgm> list = bgmMapper.selectByExample(example);
+
+        PageInfo<Bgm> pageList = new PageInfo<>(list);
+
+        PagedResult result = new PagedResult();
+        result.setTotal(pageList.getPages());
+        result.setRows(list);
+        result.setPage(page);
+        result.setRecords(pageList.getTotal());
+
+        return result;
+    }
+
+    @Override
+    public void addBgm(Bgm bgm) {
+        String bgmId = sid.nextShort();
+        bgm.setId(bgmId);
+        bgmMapper.insert(bgm);
+
+        Map<String, String> map = new HashMap<>();
+        map.put("operType", BGMOperatorTypeEnum.ADD.type);
+        map.put("path", bgm.getPath());
+
+        zkCurator.sendBgmOperator(bgmId, JsonUtils.objectToJson(map));
+    }
+
+    @Override
+    public void deleteBgm(String id) {
+        Bgm bgm = bgmMapper.selectByPrimaryKey(id);
+
+        bgmMapper.deleteByPrimaryKey(id);
+
+        Map<String, String> map = new HashMap<>();
+        map.put("operType", BGMOperatorTypeEnum.DELETE.type);
+        map.put("path", bgm.getPath());
+
+        zkCurator.sendBgmOperator(id, JsonUtils.objectToJson(map));
+
+    }
 
 }
